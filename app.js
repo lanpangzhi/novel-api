64e6f7e8e858c057b38b4927b6d7293872d6e7ff
lanpangzhi <875727617@qq.com>
2018-05-17 17:51:39 +0800

    修改index路由中间件地址

--- app.js
+++ app.js
@@ -1,20 +1,45 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+let express = require('express');
+let path = require('path');
+let cookieParser = require('cookie-parser');
+let logger = require('morgan');
+let fs = require('fs');
+let FileStreamRotator = require('file-stream-rotator'); // 日志按时间分割模块
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/users');
 
-app.use(logger('dev'));
+let app = express();
+let logDir = path.join(__dirname, 'log');
+
+// 检查是否存在logDir这个目录没有则创建
+fs.existsSync(logDir) || fs.mkdirSync(logDir);
+
+// 日志按时间分割流
+let accessLogStream = FileStreamRotator.getStream({
+    date_format: 'YYYYMMDD',
+    filename: path.join(logDir, 'access-%DATE%.log'),
+    frequency: 'daily',
+    verbose: false
+});
+
+// 日志中间件
+app.use(logger('combined', { stream: accessLogStream }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By", ' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next()
+});
+
+app.use('/index', indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
