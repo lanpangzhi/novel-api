69ded5cce24537e87ed3e1b3f54c330c378c34f4
lanpangzhi <875727617@qq.com>
2018-05-26 10:53:56 +0800

    增加上一页和下一页

--- routes/article.js
+++ routes/article.js
@@ -24,7 +24,44 @@ router.get('/', function (req, res, next) {
         body = JSON.parse(body);
 
         if (body.ok){
-            res.send(JSON.stringify({ "flag": 1, "chapter": body.chapter, "msg": "OK" }));
+            // 再次请求列表页获取上一页和下一页
+            if(req.query.id){
+                // req.query.id 编码转义
+                let id = encodeURIComponent(req.query.id);
+                let n = parseInt(req.query.n);
+                if (isNaN(n)){
+                    n = 0;
+                }
+
+                request.get(`${common.API}/atoc/${id}?view=chapters`, function (err, response, body2) {
+                    if (err) {
+                        res.send(JSON.stringify({ "flag": 0, "msg": "请求出错了..." }));
+                    }
+
+                    if (body2 == "wrong param"){
+                        res.send(JSON.stringify({ "flag": 0, "msg": "传入错误的ID..." }));
+                    }else{
+                        // 解析返回的数据
+                        body2 = JSON.parse(body2);
+                        // 检查页码是否超过小说的章节数
+                        if(n > body2.chapters.length - 1){
+                            res.send(JSON.stringify({ "flag": 0, "msg": "传入的页码过大" }));
+                        }else{
+                            // 如果有上一页或者下一页就返回link否则返回false
+                            let prev,next;
+                            body2.chapters[n - 1] ? prev = body2.chapters[n - 1].link : prev = false;
+                            body2.chapters[n + 1] ? next = body2.chapters[n + 1].link : next = false;
+
+                            if (body2.chapters.length > 0) {
+                                res.send(JSON.stringify({ "flag": 1,"id": id, "chapter": body.chapter, "prev": prev,"next": next, "msg": "OK" }));
+                            }
+                        }
+                    }
+                });
+            }else{
+                res.send(JSON.stringify({ "flag": 1, "chapter": body.chapter, "msg": "OK" }));
+            }
+            
         }else{
             res.send(JSON.stringify({ "flag": 0, "msg": "传入link有错误" }));
         }
