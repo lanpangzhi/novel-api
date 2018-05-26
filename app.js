802fd8618d649dc6ddadd2eb91a1655e4cdb8871
lanpangzhi <875727617@qq.com>
2018-05-26 10:53:32 +0800

    增加错误处理中间件

--- app.js
+++ app.js
@@ -58,4 +58,22 @@ app.use('/article', articleRouter);
 // 排行榜
 app.use('/ranking', rankingRouter);
 
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
 module.exports = app;
